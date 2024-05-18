d6b51e93e708c4d63f4e1bd3bf15f6d4d86df945
EstebanArenasG <estebanarenas777@gmail.com>
2024-05-17 19:08:47 -0500

    organizo los enrutamientos y establezco el home como principal

--- BrokerTec/src/App.jsx
+++ BrokerTec/src/App.jsx
@@ -1,16 +1,18 @@
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './screens/Home.jsx';
 import SignUp from './screens/SignUp.jsx';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path='/home' element={<Home />} />
-        <Route path='/signup' element={<SignUp />} />
-      </Routes>
-    </Router>
+    <div id='container'>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/signup' element={<SignUp />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
--- BrokerTec/src/screens/SignUp.jsx
+++ BrokerTec/src/screens/SignUp.jsx
@@ -1,16 +1,12 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
 import '../App.css';
 
 export default function SignUp() {
-  const navigate = useNavigate();
   return (
     <div id='container_signup'>
-      <h1>Estoy en Registrarse</h1>
-      <button id='signup' onClick={() => navigate('/home')}>
-        ir a home
-      </button>
+      <h1>Registrarse</h1>
+      <h3>Nombre</h3>
     </div>
   );
 }
